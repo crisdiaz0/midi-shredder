7626330f77e0349a5d7a97c2c29fefeabc513154
crisdiaz0 <ape4p2diaz.cristian@gmail.com>
2019-07-05 21:20:44 -0500

    Added the midi-json-parser library for reading the midi file. Basic input of files works and a file reader is being used to get the binary data.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { parseArrayBuffer } from 'midi-json-parser';
+
+const getFile = e => {
+	const file = e.target.files[0];
+	console.log('file: ', file);
+
+	if (file.type !== 'audio/midi') return;
+
+	const fileReader = new FileReader();
+	fileReader.readAsArrayBuffer(file);
+	console.log('fileReader: ', fileReader);
+};
 
 function App() {
 	return (
 		<div className="App">
-			<h1>midi-shredder WIP</h1>
+			<h1>midi-shredder</h1>
+			<input
+				type="file"
+				id="files"
+				name="files"
+				onInput={e => getFile(e)}
+			/>
 		</div>
 	);
 }
